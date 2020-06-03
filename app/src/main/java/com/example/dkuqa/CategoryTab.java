47c26b30867c903aec845c74af0c557149474426
DongChyeon <kimdonghyun90766@gmail.com>
2020-06-03 11:20:52 +0900

    Implement category search function

--- app/src/main/java/com/example/dkuqa/CategoryTab.java
+++ app/src/main/java/com/example/dkuqa/CategoryTab.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -14,6 +16,9 @@ public class CategoryTab extends Fragment {
     RecyclerView categoryList;   // 리사이클러
     CategoryAdapter adapter;    // 리사이클러 뷰홀더
     QuestionDatabaseManager QuestionDBManager;
+
+    EditText searchWord;
+    Button searchButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
@@ -27,6 +32,8 @@ public class CategoryTab extends Fragment {
         QuestionDBManager = QuestionDatabaseManager.getInstance(getActivity());
 
         categoryList = rootView.findViewById(R.id.categoryList);
+        searchWord = rootView.findViewById(R.id.searchWord);
+        searchButton = rootView.findViewById(R.id.serachButton);
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
         categoryList.setLayoutManager(layoutManager);
@@ -42,5 +49,21 @@ public class CategoryTab extends Fragment {
         }
         cursor.close();
         categoryList.setAdapter(adapter);   // 리싸이클러뷰 적용
+
+        searchButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Cursor cursor = QuestionDBManager.rawQuery("SELECT DISTINCT Qcategory FROM Question WHERE Qcategory LIKE " + "'%" + searchWord.getText().toString() + "%'" , null);
+                int recordCount = cursor.getCount();
+                adapter.clearItems();   // 먼저 adapter의 아이템들을 비워줘야 함
+                for (int i = 0; i < recordCount; i++) {
+                    cursor.moveToNext();
+                    String Qcategory = cursor.getString(0);
+                    adapter.addItem(new CategoryModel(Qcategory));   // 리싸이클러뷰에 question 아이템 넣기
+                }
+                cursor.close();
+                categoryList.setAdapter(adapter);   // 리싸이클러뷰 적용
+            }
+        }); // 카테고리명 기반 검색 기능
     }
 }
--- app/src/main/java/com/example/dkuqa/HomeTab.java
+++ app/src/main/java/com/example/dkuqa/HomeTab.java
@@ -12,16 +12,14 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
-
 public class HomeTab extends Fragment {
     MoreInfo moreInfo;  // 카드뷰
     static RecyclerView questionList;   // 리사이클러
     QuestionAdapter adapter;    // 리사이클러 뷰홀더
     QuestionDatabaseManager QuestionDBManager;
 
-    EditText editText;
-    Button button;
+    EditText searchWord;
+    Button searchButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
@@ -37,8 +35,8 @@ public class HomeTab extends Fragment {
 
         moreInfo = rootView.findViewById(R.id.moreInfo);
         questionList = rootView.findViewById(R.id.questionList);
-        editText = rootView.findViewById(R.id.editText);
-        button = rootView.findViewById(R.id.button);
+        searchWord = rootView.findViewById(R.id.searchWord);
+        searchButton = rootView.findViewById(R.id.searchButton);
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
         questionList.setLayoutManager(layoutManager);
@@ -71,10 +69,10 @@ public class HomeTab extends Fragment {
         }
     }); // 리사이클러뷰에 클릭리스너 추가 (카드뷰로 구현한 자세히 보기 화면 나옴)
 
-        button.setOnClickListener(new View.OnClickListener(){
+        searchButton.setOnClickListener(new View.OnClickListener(){
         @Override
         public void onClick(View v) {
-            Cursor cursor = QuestionDBManager.rawQuery("SELECT Qtitle, Qcategory FROM Question WHERE Qtitle LIKE" + "%" + editText.getText().toString() + "%" , null);
+            Cursor cursor = QuestionDBManager.rawQuery("SELECT Qtitle, Qcategory FROM Question WHERE Qtitle LIKE " + "'%" + searchWord.getText().toString() + "%'" , null);
             int recordCount = cursor.getCount();
             adapter.clearItems();   // 먼저 adapter의 아이템들을 비워줘야 함
             for (int i = 0; i < recordCount; i++) {
@@ -84,7 +82,7 @@ public class HomeTab extends Fragment {
                 adapter.addItem(new QuestionModel(Qtitle, Qcategory));   // 리싸이클러뷰에 question 아이템 넣기
             }
             cursor.close();
-            questionList.setAdapter(adapter);   // 리싸이클러뷰 적용
+            questionList.setAdapter(adapter);   // 제목 기반 검색 기능
 
             adapter.setOnItemClickListener(new OnQuestionItemClickListener() {
                 @Override
@@ -101,6 +99,6 @@ public class HomeTab extends Fragment {
                 }
             }); // 리사이클러뷰에 클릭리스너 추가 (카드뷰로 구현한 자세히 보기 화면 나옴)
         }
-        }); // 제목 기반 검색 기능
+        });
     }
 }
