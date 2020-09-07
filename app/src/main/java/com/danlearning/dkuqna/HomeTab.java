cb766bc83c59363629629e96ed2e50afe794ff5d
DongChyeon <kimdonghyun90766@gmail.com>
2020-09-07 22:12:28 +0900

    Update HomeTab using firebase firestore

--- app/src/main/java/com/danlearning/dkuqna/HomeTab.java
+++ app/src/main/java/com/danlearning/dkuqna/HomeTab.java
@@ -3,23 +3,34 @@ package com.danlearning.dkuqna;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class HomeTab extends Fragment {
     RecyclerView questionList;   // 리사이클러
     QuestionAdapter adapter;    // 리사이클러 뷰홀더
-    DatabaseManager DBManager;
 
     EditText searchWord;
     Button searchButton;
+
+    FirebaseFirestore firestore = FirebaseFirestore.getInstance();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
@@ -31,8 +42,6 @@ public class HomeTab extends Fragment {
     }
 
     private void InitUI(ViewGroup rootView) {
-        DBManager = DatabaseManager.getInstance(getActivity());
-
         questionList = rootView.findViewById(R.id.questionList);
         searchWord = rootView.findViewById(R.id.searchWord);
         searchButton = rootView.findViewById(R.id.searchButton);
@@ -41,61 +50,60 @@ public class HomeTab extends Fragment {
         questionList.setLayoutManager(layoutManager);
         adapter = new QuestionAdapter();
 
-        Cursor cursor = DBManager.rawQuery("SELECT Qtitle, Qcategory FROM Question", null);
-        int recordCount = cursor.getCount();
+        firestore.collection("questions").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot snapshot,
+                                @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_LONG).show();
+                    return;
+                }   // 에러가 발생하면 종료
 
-        for (int i = 0; i < recordCount; i++) {
-            cursor.moveToNext();
-            String Qtitle = cursor.getString(0);
-            String Qcategory = cursor.getString(1);
-            adapter.addItem(new QuestionModel(Qtitle, Qcategory));   // 리싸이클러뷰에 question 아이템 넣기
-        }
-        cursor.close();
-        questionList.setAdapter(adapter);   // 리싸이클러뷰 적용
+                adapter.clearItems();
+
+                for (QueryDocumentSnapshot doc : snapshot) {
+                    adapter.addItem(new QuestionModel(doc.getString("title"), doc.getString("category"), doc.getString("content")));
+                }
+                adapter.notifyDataSetChanged();
+                questionList.setAdapter(adapter);
+            }
+        }); // 파이어스토어 questions 컬렉션에서 데이터를 불러와 리사이클러뷰에 적용
 
         adapter.setOnItemClickListener(new OnQuestionItemClickListener() {
             @Override
             public void onItemClick(QuestionAdapter.ViewHolder holder, View view, int position) {
                 Intent intent = new Intent(getActivity(), ViewPostActivity.class);
-                Cursor cursor = DBManager.rawQuery("SELECT Qtitle, Qcontent FROM Question", null);
-                cursor.moveToPosition(position);
-                String Qtitle = cursor.getString(0);
-                String Qcontent = cursor.getString(1);
-                intent.putExtra("title", Qtitle);
-                intent.putExtra("content", Qcontent);
+
+                intent.putExtra("title", adapter.getItem(position).getTitle());
+                intent.putExtra("content", adapter.getItem(position).getContent());
                 startActivity(intent);
             }
         }); // 리사이클러뷰에 클릭리스너 추가 (카드뷰로 구현한 자세히 보기 화면 나옴)
 
-        searchButton.setOnClickListener(new View.OnClickListener(){
-        @Override
-        public void onClick(View v) {
-            Cursor cursor = DBManager.rawQuery("SELECT Qtitle, Qcategory FROM Question WHERE Qtitle LIKE " + "'%" + searchWord.getText().toString() + "%'" , null);
-            int recordCount = cursor.getCount();
-            adapter.clearItems();   // 먼저 adapter의 아이템들을 비워줘야 함
-            for (int i = 0; i < recordCount; i++) {
-                cursor.moveToNext();
-                String Qtitle = cursor.getString(0);
-                String Qcategory = cursor.getString(1);
-                adapter.addItem(new QuestionModel(Qtitle, Qcategory));   // 리싸이클러뷰에 question 아이템 넣기
-            }
-            cursor.close();
-            questionList.setAdapter(adapter);   // 제목 기반 검색 기능
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firestore.collection("questions").addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot snapshot,
+                                        @Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_LONG).show();
+                            return;
+                        }   // 에러가 발생하면 종료
 
-            adapter.setOnItemClickListener(new OnQuestionItemClickListener() {
-                @Override
-                public void onItemClick(QuestionAdapter.ViewHolder holder, View view, int position) {
-                    Intent intent = new Intent(getActivity(), ViewPostActivity.class);
-                    Cursor cursor = DBManager.rawQuery("SELECT Qtitle, Qcontent FROM Question WHERE Qtitle LIKE " + "'%" + searchWord.getText().toString() + "%'" , null);
-                    cursor.moveToPosition(position);
-                    String Qtitle = cursor.getString(0);
-                    String Qcontent = cursor.getString(1);
-                    intent.putExtra("title", Qtitle);
-                    intent.putExtra("content", Qcontent);
-                    startActivity(intent);
-                }
-            }); // 리사이클러뷰에 클릭리스너 추가
-        }
+                        adapter.clearItems();
+
+                        for (QueryDocumentSnapshot doc : snapshot) {
+                            if (doc.getString("title").contains(searchWord.getText().toString())) { // 타이틀에 정해진 키워드가 있는지 판단
+                                adapter.addItem(new QuestionModel(doc.getString("title"), doc.getString("category"), doc.getString("content")));
+                            }
+                        }
+                        adapter.notifyDataSetChanged();
+                        questionList.setAdapter(adapter);
+                    }
+                }); // 파이어스토어 questions 컬렉션에서 데이터를 불러와 리사이클러뷰에 적용
+            }
         });
     }
 }
--- app/src/main/java/com/danlearning/dkuqna/QuestionModel.java
+++ app/src/main/java/com/danlearning/dkuqna/QuestionModel.java
@@ -3,10 +3,12 @@ package com.danlearning.dkuqna;
 public class QuestionModel {
     String title;
     String category;
+    String content;
 
-    public QuestionModel(String title, String category) {
+    public QuestionModel(String title, String category, String content) {
         this.title = title;
         this.category = category;
+        this.content = content;
     }   // 질문 생성자
 
     public String getTitle() {
@@ -20,4 +22,12 @@ public class QuestionModel {
     public String getCategory() { return category; }
 
     public void setCategory(String category) { this.category = category; }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
